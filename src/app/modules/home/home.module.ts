7f710ef71b0192bba223d2b797d2a1acd5c7c7ba
jair <jairdeyner730791@gmail.com>
2022-12-28 13:31:05 -0500

    ProductosComponent added

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    CarouselComponent
+    CarouselComponent,
+    ProductosComponent
   ],
   exports:[
     HomePageComponent
