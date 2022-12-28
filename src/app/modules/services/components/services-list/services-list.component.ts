f0232cfda755e9bb6366771f6f4f131582a65270
jair <jairdeyner730791@gmail.com>
2022-12-28 14:03:21 -0500

    ServicesListComponent added

---
+++ src/app/modules/services/components/services-list/services-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services-list',
+  templateUrl: './services-list.component.html',
+  styleUrls: ['./services-list.component.css']
+})
+export class ServicesListComponent {
+
+}
--- src/app/modules/services/services.module.ts
+++ src/app/modules/services/services.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ServicePageComponent } from './pages/service-page/service-page.component';
 import { ServiceDescriptionComponent } from './components/service-description/service-description.component';
+import { ServicesListComponent } from './components/services-list/services-list.component';
 
 
 
 @NgModule({
   declarations: [
     ServicePageComponent,
-    ServiceDescriptionComponent
+    ServiceDescriptionComponent,
+    ServicesListComponent
   ],
   exports: [
     ServicePageComponent
