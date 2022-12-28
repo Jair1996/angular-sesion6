70803ee3c91d9e7847b520d156a28db9a4177111
jair <jairdeyner730791@gmail.com>
2022-12-28 13:51:17 -0500

    MissionVissionComponent added

--- src/app/modules/about-us/about-us.module.ts
+++ src/app/modules/about-us/about-us.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutUsPageComponent } from './pages/about-us-page/about-us-page.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { MissionVissionComponent } from './components/mission-vission/mission-vission.component';
 
 
 
 @NgModule({
   declarations: [
     AboutUsPageComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    MissionVissionComponent
   ],
   exports: [
     AboutUsPageComponent
---
+++ src/app/modules/about-us/components/mission-vission/mission-vission.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mission-vission',
+  templateUrl: './mission-vission.component.html',
+  styleUrls: ['./mission-vission.component.css']
+})
+export class MissionVissionComponent {
+
+}
