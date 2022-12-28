20d0cd890c8d4f79c3ec33a5a453e51a41e1f527
jair <jairdeyner730791@gmail.com>
2022-12-28 14:01:09 -0500

    ServiceDescriptionComponent added

--- src/app/modules/services/services.module.ts
+++ src/app/modules/services/services.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ServicePageComponent } from './pages/service-page/service-page.component';
+import { ServiceDescriptionComponent } from './components/service-description/service-description.component';
 
 
 
 @NgModule({
   declarations: [
-    ServicePageComponent
+    ServicePageComponent,
+    ServiceDescriptionComponent
   ],
   exports: [
     ServicePageComponent
