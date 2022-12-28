c7644314c3b9d3702e47a8dd2d2aa5232367e820
jair <jairdeyner730791@gmail.com>
2022-12-28 13:12:20 -0500

    CarouselComponent added

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    CarouselComponent
   ],
   exports:[
     HomePageComponent
