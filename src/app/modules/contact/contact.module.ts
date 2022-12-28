1190942af0c2ab7b58f51f85bda214191d0fe281
jair <jairdeyner730791@gmail.com>
2022-12-28 14:18:25 -0500

    ContacForm added

--- src/app/modules/contact/contact.module.ts
+++ src/app/modules/contact/contact.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { ContactContentComponent } from './components/contact-content/contact-content.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 
 
 
 @NgModule({
   declarations: [
     ContactPageComponent,
-    ContactContentComponent
+    ContactContentComponent,
+    ContactFormComponent
   ],
   exports: [
     ContactPageComponent
